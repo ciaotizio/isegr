083eafefe334e77a51552787125db4efc97117a7
ciaotizio <ciaotizio59@gmail.com>
2024-05-02 21:16:51 +0200

    Add files via upload

---
+++ inserimento.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inserimento nel Database</title>
+</head>
+<body>
+    <?php
+        $conn = new mysqli("localhost", "root", "", "")
+
+    ?>
+</body>
+</html>
